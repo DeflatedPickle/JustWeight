cc6b02135163d99db19079722f9234eabf2ca95a
Dibble <DeflatedPickle@gmail.com>
2018-05-20 11:40:47 +0100

    Added Reference object

---
+++ src/main/kotlin/com/deflatedpickle/justweight/Reference.kt
@@ -0,0 +1,12 @@
+package com.deflatedpickle.justweight
+
+object Reference {
+    const val MOD_ID = "justweight"
+    const val NAME = "JustWeight"
+    // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
+    const val VERSION = "1.12.2-1.0.0.0"
+    const val ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]"
+
+    const val DEPENDENCIES = "required-after:forgelin"
+    const val ADAPTER = "net.shadowfacts.forgelin.KotlinAdapter"
+}
